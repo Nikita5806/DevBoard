32ab093e95a7822d28c13f31682d4dd1596a97ba
Nikita <nikita.ns321ns@gmail.com>
2026-03-16 10:27:47 +0530

    to ensure all files are commited

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -59,6 +59,7 @@ const login = async (req, res) => {
 
     // Check password
     const isMatch = await bcrypt.compare(password, user.password)
+    console.log(isMatch)
     if (!isMatch) {
       return res.status(400).json({ message: 'Invalid credentials' })
     }
